7be311b8871459f8f2eb3ecb3a5256959625a067
Thiago Moraes <thiago.moraes582@gmail.com>
2021-08-21 22:20:38 -0300

    criação da lista de contas corrente

---
+++ ByteBank/ByteBank.SistemaAgencia/ListaContas.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class ListaContas
+    {
+        public ListaContas(params ContaCorrente[] contaCorrentes)
+        {
+
+        }
+    }
+}
